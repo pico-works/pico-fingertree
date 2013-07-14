330bd0d126ecb21374b4e2574ea6046838db4266
John Ky <newhoggy@gmail.com>
2013-07-15 01:08:32 +1000

    Implement TreeSeq.

--- cj/src/main/scala/fingertree/TreeSeq.scala
+++ cj/src/main/scala/fingertree/TreeSeq.scala
@@ -7,11 +7,17 @@ import Syntax._
 case class TreeSeq[+A](tree: FingerTree[Int@@Size, A@@Elem]) {
   def size: Int = ToMeasuredOps(tree).measure
   
-  def splitAt(i: Int): TreeSeq[A] = ???
+  def splitAt(i: Int): (TreeSeq[A], TreeSeq[A]) = tree.split(size < _) match {
+    case (l, r) => (TreeSeq(l), TreeSeq(r))
+  }
+  
+  def apply(i: Int): A = tree.splitTree(i < _)(Tag[Int, Size](0)) match {
+    case Split(_, x, _) => x
+  }
 }
 
 object TreeSeq {
-  def empty: TreeSeq[Nothing] = TreeSeq[Nothing](Empty())
+  val empty: TreeSeq[Nothing] = new TreeSeq[Nothing](Empty())
   
-  
+  def apply[A](values: A*): TreeSeq[A] = new TreeSeq[A](values.map(Tag[A, Elem](_)).toList.toTree[Int@@Size](MeasuredElemSize[A@@Elem]))
 }
