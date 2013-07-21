8686393d9af12775a22a6873baa5d4e9c77638fa
John Ky <newhoggy@gmail.com>
2013-07-22 01:27:48 +1000

    Tidy up.

--- cj/src/main/scala/fingertree/ReduceOps.scala
+++ cj/src/main/scala/fingertree/ReduceOps.scala
@@ -6,7 +6,6 @@ import scalaz.syntax.Ops
 trait ReduceOps[F[_], A] extends Ops[F[A]] {
   implicit def F: Reduce[F]
 
-  // Note: Renamed from toList to asList to avoid conflict with built-in Scala toList method
   def asList: List[A] = F.reduceR[A, List[A]](_ :: _)(self, Nil)
   
   def asTree[V](implicit M: Measured[V, A]): FingerTree[V, A] = F.reduceR[A, FingerTree[V, A]](_ +: _)(self, Empty())
--- cj/src/main/scala/fingertree/package.scala
+++ cj/src/main/scala/fingertree/package.scala
@@ -1,9 +1,14 @@
-package object fingertree {
-  import scalaz.@@
-  import scalaz.Tags._
-  import Syntax._
+import scala.language.higherKinds
+import scalaz.@@
+import scalaz.Tags._
+import fingertree.Syntax._
 
+package object fingertree {
   def !!!(): Nothing = throw new UnsupportedOperationException
+
+  def asList[F[_], A](fa: F[A])(implicit F: Reduce[F]): List[A] = F.reduceR[A, List[A]](_ :: _)(fa, Nil)
+  
+  def asTree[F[_], V, A](fa: F[A])(implicit F: Reduce[F], M: Measured[V, A]): FingerTree[V, A] = F.reduceR[A, FingerTree[V, A]](_ +: _)(fa, Empty())
 
   def asList[V, A](digit: Digit[V, A])(implicit F0: Reduce[Digit.α[V]#α]): List[A] = {
     type FV[+A] = FingerTree[V, A]
