2c6ced6bfaa1755dad19c6a004e5610b917114ae
John Ky <newhoggy@gmail.com>
2013-07-22 01:12:47 +1000

    Tidy up.

--- cj/src/main/scala/fingertree/ToReduceOps.scala
+++ cj/src/main/scala/fingertree/ToReduceOps.scala
@@ -4,7 +4,7 @@ import scala.language.higherKinds
 import scala.language.implicitConversions
 
 trait ToReduceOps {
-  implicit def ToReduceOps[F[_], A](v: F[A])(implicit F0: Reduce[F]) = new ReduceOps[F,A] {
+  implicit def ToReduceOps[F[_], A](v: F[A])(implicit F0: Reduce[F]) = new ReduceOps[F, A] {
     def self = v
     implicit def F: Reduce[F] = F0
   }
