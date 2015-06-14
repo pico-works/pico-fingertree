7bce57314ce7724cee1d5e7c543651ca9d910d76
John Ky <jky@arbor.net>
2015-06-14 18:59:38 +1000

    Tagging and untagged of higher kinded types.

--- cj/src/main/scala/org/pico/tag/package.scala
+++ cj/src/main/scala/org/pico/tag/package.scala
@@ -1,14 +1,22 @@
 package org.pico
 
+import scala.language.higherKinds
 import scalaz.@@
 
 package object tag {
-  implicit class TaggedOps[A, T](self: A @@ T) {
-    def covariant[B >: A]: B @@ T = {
-      val a: A = ???
-      val b: B = a
+  implicit class TaggedF1Ops[V, T, F[_]](val self: F[V @@ T]) extends AnyVal {
+    def untagged: F[V] = self.asInstanceOf[F[V]]
+  }
 
-      ???
-    }
+  implicit class TaggedF2Ops[V, T, F[_, _], A](val self: F[A, V @@ T]) extends AnyVal {
+    def untagged: F[A, V] = self.asInstanceOf[F[A, V]]
+  }
+
+  implicit class UntaggedF1Ops[V, F[_]](val self: F[V]) extends AnyVal {
+    def tagged[T]: F[V @@ T] = self.asInstanceOf[F[V @@ T]]
+  }
+
+  implicit class UntaggedF2Ops[V, T, F[_, _], A](val self: F[A, V @@ T]) extends AnyVal {
+    def tagged[T]: F[A, V @@ T] = self.asInstanceOf[F[A, V @@ T]]
   }
 }
