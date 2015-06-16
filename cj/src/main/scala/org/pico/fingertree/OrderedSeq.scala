78196047d72b12d2dab53402d3ea7796ab0e328b
John Ky <jky@arbor.net>
2015-06-16 23:15:05 +1000

    Implement OrderedSeq.

--- cj/src/main/scala/org/pico/fingertree/OrderedSeq.scala
+++ cj/src/main/scala/org/pico/fingertree/OrderedSeq.scala
@@ -1,25 +1,41 @@
 package fingertree
 
-import scalaz.Monoid
-import scalaz.Order
-import scalaz.@@
+import org.pico.collection.Measured
+import org.pico.tag._
 
-//trait Key[+K]
-//case object NoKey extends Key[Nothing]
-//case class SomeKey[K](key: K) extends Key[K]
-//
-//trait OrderedSeqImplicits extends Implicits {
-//  implicit def KeyMonoid[A: Order](): Monoid[Key[A]] = new Monoid[Key[A]] {
-//    override def zero: Key[A] = NoKey
-//    override def append(l: Key[A], r: => Key[A]): Key[A] = (l, r) match {
-//      case (l, NoKey) => l
-//      case (l, r) => r
-//    }
-//  }
-//}
-//
-//case class OrderedSeq[K, +A <: K](tree: FingerTree[Key[K], A@@Elem]) {
-//  def +:[B >: A <: K](a: B): OrderedSeq[K, B] = OrderedSeq(tree)
-//}
-//
-//object OrderedSeq extends OrderedSeqImplicits
+import scalaz.{@@, Monoid, Order, Tag}
+
+trait Key[+K]
+case object NoKey extends Key[Nothing]
+case class SomeKey[K](key: K) extends Key[K]
+
+trait OrderedSeqImplicits extends Implicits {
+  implicit def KeyMonoid[A: Order](): Monoid[Key[A]] = new Monoid[Key[A]] {
+    override def zero: Key[A] = NoKey
+
+    override def append(l: Key[A], r: => Key[A]): Key[A] = (l, r) match {
+      case (l, NoKey) => l
+      case (l, r) => r
+    }
+  }
+
+  implicit def MeasuredElemSize[K: Order, A] = new Measured[Key[K], A@@Elem] {
+    override implicit def monoid: Monoid[Key[K]] = KeyMonoid[K]
+
+    override def measure(tree: A@@Elem): Key[K] = ???
+  }
+}
+
+case class OrderedSeq[K: Order, +A <: K](tree: FingerTree[Key[K], A]) {
+  import OrderedSeq._
+
+  def +:[B >: A <: K](a: B): OrderedSeq[K, B] = {
+    OrderedSeq((Tag.of[Elem](a) +: (tree: FingerTree[Key[K], B]).tagged[Elem]).untagged)
+  }
+
+  def :+[B >: A <: K](a: B): OrderedSeq[K, B] = {
+    OrderedSeq(((tree: FingerTree[Key[K], B]).tagged[Elem] :+ Tag.of[Elem](a)).untagged)
+  }
+}
+
+object OrderedSeq extends OrderedSeqImplicits
