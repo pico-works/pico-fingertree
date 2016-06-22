ccd1f1a4b75dd9901490811139fe67c624e0b233
John Ky <newhoggy@gmail.com>
2016-06-23 08:24:50 +1000

    Modernize build

--- project/Build.scala
+++ project/Build.scala
@@ -2,32 +2,27 @@ import sbt._
 import Keys._
 
 object Build extends sbt.Build with Version {
+  val pico_kind         = "io.john-ky"  %%  "pico-kind"                 % "0.0.1-dd87cb4"
+  val scalaz_core       = "org.scalaz"  %%  "scalaz-core"               % "7.2.0"
+
+  val specs2_core       = "org.specs2"  %%  "specs2-core"               % "3.7.2"
+
   implicit class ProjectOps(self: Project) {
-    def standard: Project = {
+    def standard(theDescription: String) = {
       self
-          .settings(organization := "io.john-ky")
-          .settings(scalacOptions := Seq(
-              "-feature",
-              "-deprecation",
-              "-unchecked",
-              "-Xlint",
-              "-Yrangepos",
-              "-encoding",
-              "utf8"))
-          .settings(scalacOptions in (console) += "-Yrangepos")
-    }
-
-    def notPublished: Project = {
-      self
-          .settings(publish := {})
-          .settings(publishArtifact := false)
-    }
-
-    def published: Project = {
-      self
-          .settings(publishTo := Some("Scalap Releases" at "s3://dl.john-ky.io/maven/releases"))
+          .settings(scalacOptions in Test ++= Seq("-Yrangepos"))
+          .settings(publishTo := Some("Releases" at "s3://dl.john-ky.io/maven/releases"))
+          .settings(description := theDescription)
           .settings(isSnapshot := true)
+          .settings(resolvers += Resolver.sonatypeRepo("releases"))
+          .settings(addCompilerPlugin("org.spire-math" % "kind-projector" % "0.8.0" cross CrossVersion.binary))
     }
+
+    def notPublished = self.settings(publish := {}).settings(publishArtifact := false)
+
+    def libs(modules: ModuleID*) = self.settings(libraryDependencies ++= modules)
+
+    def testLibs(modules: ModuleID*) = self.libs(modules.map(_ % "test"): _*)
   }
   
   lazy val `pico-fake` = Project(id = "pico-fake", base = file("pico-fake"))
@@ -35,13 +30,10 @@ object Build extends sbt.Build with Version {
       .testLibs(specs2_core)
 
   lazy val `pico-fingertree` = Project(id = "pico-fingertree", base = file("pico-fingertree"))
-    .standard
-    .published
-    .settings(libraryDependencies ++= Seq(
-      "org.scalaz" %% "scalaz-core" % "7.1.2",
-      "io.john-ky" %% "pico-kind"   % "0.0.1-dd87cb4"))
+      .standard("Tiny finger tree library")
+      .libs(scalaz_core, pico_kind)
 
   lazy val root = Project(id = "all", base = file("."))
-    .notPublished
-    .aggregate(`pico-fingertree`, `pico-fake`)
+      .notPublished
+      .aggregate(`pico-fingertree`, `pico-fake`)
 }
