47b193f861ff9c41f89a4818c9089dbc013c6cdc
John Ky <newhoggy@gmail.com>
2016-07-05 08:19:04 +1000

    Modernise build

--- build.sbt
+++ build.sbt
@@ -1,9 +1,12 @@
-scalaVersion in ThisBuild := "2.10.5"
+organization in ThisBuild := "org.pico"
 
-crossScalaVersions := Seq("2.10.5", "2.11.6")
+scalaVersion in ThisBuild := "2.11.8"
+
+crossScalaVersions in ThisBuild := Seq("2.10.6", "2.11.8")
+
+version in ThisBuild := Process("./version.sh").lines.head.trim
 
 resolvers in ThisBuild ++= Seq(
-    "bintray/non"     at "http://dl.bintray.com/non/maven",
-    "dl-john-ky"      at "http://dl.john-ky.io/maven/releases")
-
-version in ThisBuild := buildVersion
+  "bintray/non"           at "http://dl.bintray.com/non/maven",
+  "dl-john-ky-releases"   at "http://dl.john-ky.io/maven/releases",
+  "dl-john-ky-snapshots"  at "http://dl.john-ky.io/maven/snapshots")
--- project/Build.scala
+++ project/Build.scala
@@ -1,11 +1,8 @@
+import sbt.Keys._
 import sbt._
-import Keys._
 
-object Build extends sbt.Build with Version {
-  val pico_kind         = "io.john-ky"  %%  "pico-kind"                 % "0.0.1-dd87cb4"
-  val scalaz_core       = "org.scalaz"  %%  "scalaz-core"               % "7.2.0"
-
-  val specs2_core       = "org.specs2"  %%  "specs2-core"               % "3.7.2"
+object Build extends sbt.Build {  
+  val specs2_core               = "org.specs2"      %%  "specs2-core"               % "3.7.2"
 
   implicit class ProjectOps(self: Project) {
     def standard(theDescription: String) = {
@@ -14,8 +11,6 @@ object Build extends sbt.Build with Version {
           .settings(publishTo := Some("Releases" at "s3://dl.john-ky.io/maven/releases"))
           .settings(description := theDescription)
           .settings(isSnapshot := true)
-          .settings(resolvers += Resolver.sonatypeRepo("releases"))
-          .settings(addCompilerPlugin("org.spire-math" % "kind-projector" % "0.8.0" cross CrossVersion.binary))
     }
 
     def notPublished = self.settings(publish := {}).settings(publishArtifact := false)
@@ -24,16 +19,16 @@ object Build extends sbt.Build with Version {
 
     def testLibs(modules: ModuleID*) = self.libs(modules.map(_ % "test"): _*)
   }
-  
+
   lazy val `pico-fake` = Project(id = "pico-fake", base = file("pico-fake"))
       .standard("Fake project").notPublished
       .testLibs(specs2_core)
 
   lazy val `pico-fingertree` = Project(id = "pico-fingertree", base = file("pico-fingertree"))
-      .standard("Tiny finger tree library")
-      .libs(scalaz_core, pico_kind)
+      .standard("Finger Tree library")
+      .testLibs(specs2_core)
 
-  lazy val root = Project(id = "all", base = file("."))
+  lazy val all = Project(id = "pico-fingertree-project", base = file("."))
       .notPublished
       .aggregate(`pico-fingertree`, `pico-fake`)
 }
